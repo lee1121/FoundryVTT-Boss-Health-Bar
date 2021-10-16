74847243376e5560607bebf316def5bdd13b4660
lee1121 <lee1121@users.noreply.github.com>
2021-10-16 02:03:51 +0200

    Added support for a second health bar

--- scripts/bossHealthBar.js
+++ scripts/bossHealthBar.js
@@ -1,10 +1,11 @@
-const hpDebounce = debounce((val) => game.settings.set('BossHealthBar', 'progress', val), 1000);
+const hpDebounceOne = debounce((valOne) => game.settings.set('BossHealthBar', 'hpValueOne', valOne), 1000);
+const hpDebounceTwo = debounce((valTwo) => game.settings.set('BossHealthBar', 'hpValueTwo', valTwo), 1000);
 
 export default class BossHealthBar extends FormApplication {
 	
   constructor() {
     super();
-	console.log("Boss constructor call start");
+		console.log("Boss constructor call start");
   }
   
   static get defaultOptions() {
@@ -20,27 +21,53 @@ export default class BossHealthBar extends FormApplication {
   
   getData() {
     // Return data to the template
-    let progress = game.settings.get('BossHealthBar', 'progress');
-    let visible = game.settings.get('BossHealthBar', 'visible');
     let isGM = game.user.isGM;
-    let bossNameLabel = game.settings.get('BossHealthBar', 'bossNameLabel');
+    let hpValueOne = game.settings.get('BossHealthBar', 'hpValueOne');
+    let hpValueTwo = game.settings.get('BossHealthBar', 'hpValueTwo');
+    let visible = game.settings.get('BossHealthBar', 'visible');
+    let secondBarVisible = game.settings.get('BossHealthBar', 'secondBarVisible');
+    let bossNameLabelOne = game.settings.get('BossHealthBar', 'bossNameLabelOne');
+    let bossNameLabelTwo = game.settings.get('BossHealthBar', 'bossNameLabelTwo');
     return {
       isGM,
-      progress,
+      hpValueOne,
+      hpValueTwo,
       visible,
-			bossNameLabel
+			secondBarVisible,
+			bossNameLabelOne,
+			bossNameLabelTwo,
     };
   }
   
   selectSelectedToken() {
 		if(canvas.tokens.controlled && canvas.tokens.controlled[0]) {
-			this.selectedToken = canvas.tokens.controlled[0];
-			ui.notifications.info("Selected boss bar for token " + this.selectedToken.name);
-			this.updateHP();
+			this.selectedTokens = canvas.tokens.controlled;
 			
-			game.settings.set('BossHealthBar', 'bossNameLabel', this.selectedToken.name);
+			for (let controlledIndex = 0; controlledIndex < canvas.tokens.controlled.length; controlledIndex++) {
+				ui.notifications.info("Selected boss bar for token " + this.selectedTokens[controlledIndex].name);
+			}
 			
-			return this.selectedToken;
+			this.updateHP(0);
+			
+			console.log("secondBarVisible before selection: " + game.settings.get('BossHealthBar', 'secondBarVisible'))
+			
+			game.settings.set('BossHealthBar', 'bossNameLabelOne', this.selectedTokens[0].name);
+			if(canvas.tokens.controlled.length > 1)
+			{
+				this.updateHP(1);
+				
+				game.settings.set('BossHealthBar', 'bossNameLabelTwo', this.selectedTokens[1].name);
+				
+				game.settings.set('BossHealthBar', 'secondBarVisible', true);
+				console.log("Multiple bosses selected");
+			}
+			else
+			{
+				game.settings.set('BossHealthBar', 'secondBarVisible', false);
+				console.log("1 boss selected");
+			}
+			
+			return this.selectedTokens;
 		}
 		else {
 			ui.notifications.error("No token selected!");
@@ -48,14 +75,32 @@ export default class BossHealthBar extends FormApplication {
   }
 	
 	toggleVisibility() {
-		game.settings.set('BossHealthBar', 'visible', game.settings.get('BossHealthBar', 'visible') == false);
+		game.settings.set('BossHealthBar', 'visible', game.settings.get('BossHealthBar', 'visible') === false);
 	}
 	
-	updateHP()
+	toggleSecondBarVisibility() {
+		game.settings.set('BossHealthBar', 'secondBarVisible', game.settings.get('BossHealthBar', 'secondBarVisible') === false);
+	}
+	
+	updateAllHP()
 	{
-		let value = this.selectedToken.actor.data.data.attributes.hp.value / this.selectedToken.actor.data.data.attributes.hp.max * 100;
+		for (let controlledIndex = 0; controlledIndex < this.selectedTokens.length; controlledIndex++) {
+			this.updateHP(controlledIndex);
+		}
+	}
+	
+	updateHP(index)
+	{
+		let value = this.selectedTokens[index].actor.data.data.attributes.hp.value / this.selectedTokens[index].actor.data.data.attributes.hp.max * 100;
 		
-		hpDebounce(value);
+		if(index == 0)
+		{
+			hpDebounceOne(value);
+		}
+		else if(index == 1)
+		{
+			hpDebounceTwo(value);
+		}
 	}
 
   activateListeners(html) {
@@ -63,6 +108,7 @@ export default class BossHealthBar extends FormApplication {
   }
 
   async _updateObject(event, formData) {
-    game.settings.set('BossHealthBar', 'progress', formData.progress);
+    game.settings.set('BossHealthBar', 'hpValueOne', formData.hpValueOne);
+    game.settings.set('BossHealthBar', 'hpValueTwo', formData.hpValueTwo);
   }
 }
--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,8 @@
 import BossHealthBar from './bossHealthBar.js';
 
 Hooks.once('init', async () => {
-  await game.settings.register('BossHealthBar', 'progress', {
-    name: 'progress',
+  await game.settings.register('BossHealthBar', 'hpValueOne', {
+    name: 'hpValueOne',
     scope: 'world',
     config: false,
     type: Number,
@@ -13,7 +13,23 @@ Hooks.once('init', async () => {
       step: 1
     },
     onChange: value => {
-			let pg = $('#boss-health [name=progress]');
+			let pg = $('#boss-health [name=hpValueOne]');
+			pg.css('width', value+'%');
+    }
+  });
+  await game.settings.register('BossHealthBar', 'hpValueTwo', {
+    name: 'hpValueTwo',
+    scope: 'world',
+    config: false,
+    type: Number,
+    default: 0,
+    range: {
+      min: 0,
+      max: 500,
+      step: 1
+    },
+    onChange: value => {
+			let pg = $('#boss-health [name=hpValueTwo]');
 			pg.css('width', value+'%');
     }
   });
@@ -28,14 +44,35 @@ Hooks.once('init', async () => {
       else $('#boss-health').removeClass('visible');
     }
   });
-  await game.settings.register('BossHealthBar', 'bossNameLabel', {
-    name: 'bossNameLabel',
+  await game.settings.register('BossHealthBar', 'secondBarVisible', {
+    name: 'secondBarVisible',
+    scope: 'world',
+    config: false,
+    type: Boolean,
+    default: false,
+    onChange: value => {
+      if (value) $('#boss-health-bar-two').addClass('secondBarVisible');
+      else $('#boss-health-bar-two').removeClass('secondBarVisible');
+    }
+  });
+  await game.settings.register('BossHealthBar', 'bossNameLabelOne', {
+    name: 'bossNameLabelOne',
     scope: 'world',
     config: false,
     type: String,
     default: 'Piet the Boss',
     onChange: value => {
-			bossNameLabel.innerText = value;
+			bossNameLabelOne.innerText = value;
+    }
+  });
+  await game.settings.register('BossHealthBar', 'bossNameLabelTwo', {
+    name: 'bossNameLabelTwo',
+    scope: 'world',
+    config: false,
+    type: String,
+    default: 'Piet the Boss',
+    onChange: value => {
+			bossNameLabelTwo.innerText = value;
     }
   });
 });
@@ -46,8 +83,20 @@ Hooks.once('ready', () => {
   
 Hooks.on('updateToken', (token, data, options, id) => {
 	
-	if(window.BossHealthBar && window.BossHealthBar.selectedToken && data._id == window.BossHealthBar.selectedToken.id)
+	if(window.BossHealthBar && window.BossHealthBar.selectedTokens)
 	{
-		window.BossHealthBar.updateHP();
+		let matchesAnyID = false;
+		
+		for (let controlledIndex = 0; controlledIndex < window.BossHealthBar.selectedTokens.length; controlledIndex++) {
+			if(data._id == window.BossHealthBar.selectedTokens[controlledIndex].id)
+			{
+				matchesAnyID = true;
+			}
+		}
+		
+		if(matchesAnyID)
+		{
+			window.BossHealthBar.updateAllHP();
+		}
 	}
 })
